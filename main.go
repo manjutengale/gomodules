d4f61a1fe5b99b4f4c4c4f9c0bc4a2f3312f981e
manjutengale <manjutengale9@gmail.com>
2020-08-17 20:59:22 +0530

    version 2

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package gomodules
 import "fmt"
 
 func Add(a, b int) int {
-	fmt.Println("test")
+	fmt.Println("addition")
 	return a + b
+}
+func Sub(a, b int) int {
+	fmt.Println("sub")
+	return a - b
 }
